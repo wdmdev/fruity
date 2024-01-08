345da477b44fc2b6e369319e64ef66aaeb7e4a12
William Marstrand <wdmdevelop@gmail.com>
2024-01-08 21:50:01 +0100

    moved Hydra conf

--- src/fruity/train.py
+++ src/fruity/train.py
@@ -63,7 +63,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     return metric_dict, object_dict
 
 
-@hydra.main(version_base="1.2", config_path="conf", config_name="train.yaml")
+@hydra.main(version_base="1.2", config_path="../../conf", config_name="train.yaml")
 def main(cfg: DictConfig) -> Optional[float]:
 
     # train the model
